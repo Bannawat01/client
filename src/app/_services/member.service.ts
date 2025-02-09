08f7819d348042789ba9f0f570155197d0df2702
Bannawat <109212993+Bannawat01@users.noreply.github.com>
2025-02-09 22:55:19 +0700

    edit

--- src/app/_services/member.service.ts
+++ src/app/_services/member.service.ts
@@ -1,9 +1,11 @@
-import { HttpClient, HttpParams } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { inject, Injectable, signal } from '@angular/core'
 import { environment } from '../../environments/environment'
 import { default_paginator, Paginator, UserQueryPagination } from '../_models/pagination'
 import { User } from '../_models/user'
 import { cacheManager } from '../_helper/cache'
+import { parseQuery } from '../_helper/helper'
+
 
 type dataCategory = 'members' | 'follower' | 'following'
 @Injectable({
@@ -26,28 +28,16 @@ export class MemberService {
     }
 
     console.log(`load ${category} from server`)
-
-    // Create proper HTTP params
-    let params = new HttpParams()
-    if (pagination.pageSize) params = params.append('pageSize', pagination.pageSize.toString())
-    if (pagination.currentPage) params = params.append('currentPage', pagination.currentPage.toString())
-    if ('username' in pagination && pagination.username) params = params.append('username', pagination.username)
-    if ('looking_for' in pagination && pagination.looking_for) params = params.append('looking_for', pagination.looking_for)
-    if ('min_age' in pagination && pagination.min_age) params = params.append('min_age', pagination.min_age.toString())
-    if ('max_age' in pagination && pagination.max_age) params = params.append('max_age', pagination.max_age.toString())
-
-    this.http.get<Paginator<UserQueryPagination, User>>(`${this.url}user`, { params }).subscribe({
+    const url = this.url + 'user/' + parseQuery(pagination)
+    this.http.get<Paginator<UserQueryPagination, User>>(url).subscribe({
       next: (response) => {
         key = cacheManager.createKey(pagination)
         cacheManager.save(key, category, response)
         this.paginator.set(response)
-      },
-      error: (error) => {
-        console.error('Error fetching data:', error)
       }
     })
-  }
 
+  }
   getMember() {
     this.getData('members')
   }
